fb662c688e9e46ee017d1e327829b48523091488
Saravanan_27 <jacksaran243@gmail.com>
2026-04-09 13:36:10 +0530

    new hnages

--- jobportalbackend/jobportal/job_portal_backend/models.py
+++ jobportalbackend/jobportal/job_portal_backend/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Job(models.Model):
+    job_title=models.CharField(max_length=100)
+    description=models.TextField()
+    salary_range=models.CharField(max_length=50,blank=True)
+    posted_on=models.DateTimeField(auto_now_add=True)
+    created_by=models.ForeignKey(User,on_delete=models.CASCADE)
+    company=models.CharField(max_length=50)
+    location=models.CharField(max_length=30)
+
+class Application(models.Model):
+    job=models.ForeignKey(Job,on_delete=models.CASCADE)
+    applicant=models.ForeignKey(User,on_delete=models.CASCADE)
+    STATUS_CHOICE=(
+        ('pending','Pending'),
+        ('rejected','Rejected'),
+        ('shortlisted','Shortlisted'),
+        ('hired','Hired')
+    )
+    status=models.CharField(choices=STATUS_CHOICE,max_length=20,default='pending')
+    applied_on=models.DateTimeField(auto_now_add=True)
--- jobportalbackend/jobportal/jobportal/settings.py
+++ jobportalbackend/jobportal/jobportal/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job_portal_backend',
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobportal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
